e65a728e044fade24f5b5356f4cdc75838022f6b
z9877969 <z9877969@ukr.net>
2024-05-14 23:56:11 +0300

    adds sections fields to page model

--- models/page.js
+++ models/page.js
@@ -2,15 +2,41 @@ const { Schema, model } = require('mongoose');
 
 const pageSchema = new Schema(
   {
+    headerSection: {
+      factPhone: String,
+      displayingPhone: String,
+    },
+    heroSection: {
+      title: String,
+      descr: String,
+    },
     videoSection: {
+      title: String,
       videoUrl: String,
     },
+    aboutSection: {
+      title: String,
+      accent: {
+        type: String,
+        default: null,
+      },
+      descr: String
+    },
     sertificateSection: {
+      title: String,
+      descr: String,
       list: Array,
     },
+    quote1Section: {},
     myHelpSection: {
       list: Array,
     },
+    problemSection: {},
+    reserveSection: {},
+    quote2Section: {},
+    blogSection: {},
+    fAQSection: {},
+    footerSection: {},
   },
   {
     versionKey: false,
